867633cda19f1f9eea6bcba4f69f68d9e7c068d6
AndreyGH-lab <159480133+AndreyGH-lab@users.noreply.github.com>
2025-05-22 10:58:07 +0300

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,6 @@ from utils import load_data
 import random
 
 def create_masks(src, tgt):
-    # Предполагаем, что src и tgt имеют форму (batch_size, seq_len)
     src_key_padding_mask = (src == 0)  # Форма: (batch_size, seq_len)
     tgt_key_padding_mask = (tgt == 0)  # Форма: (batch_size, seq_len)
     tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.size(1)).to(src.device)
@@ -18,11 +17,11 @@ def train_model(model, data, nhead=4, epochs=60, learning_rate=0.001):
     criterion = nn.CrossEntropyLoss(ignore_index=0)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    batch_size = 32  # Задаем размер батча
+    batch_size = 32  
     for epoch in range(epochs):
         model.train()
         total_loss = 0
-        random.shuffle(data)  # Перемешиваем данные
+        random.shuffle(data)  
         for i in range(0, len(data), batch_size):
             batch = data[i:i + batch_size]
             src_batch = torch.stack([pair[0] for pair in batch]).to(device)  # Форма: (batch_size, seq_len)
@@ -41,16 +40,14 @@ def train_model(model, data, nhead=4, epochs=60, learning_rate=0.001):
     return total_loss / (len(data) // batch_size)
 
 if __name__ == "__main__":
-    # Загрузка и подготовка данных
     train_data = load_data("data.txt")
     vocab = Vocabulary()
     vocab.build_vocab([pair[0] for pair in train_data] + [pair[1] for pair in train_data])
     torch.save(vocab, "vocab.pt")
 
-    # Преобразование текста в тензоры
     train_data = [(vocab.text_to_tensor(src), vocab.text_to_tensor(tgt)) for src, tgt in train_data]
 
-    # Инициализация и обучение модели
+
     nhead = 4
     model = TransformerModel(vocab_size=len(vocab.get_itos()), d_model=512, nhead=nhead, num_layers=6)
     losses = train_model(model, train_data, nhead=nhead)
